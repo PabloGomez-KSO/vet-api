67b77d5b9f1cc6db16875146c1feda2f3f9ac3e9
MrPauL96 <Pablo.Gomez@kinesso.com>
2025-02-12 14:37:53 -0500

    add pets array example

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -2,9 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('pets')
 export class PetsController {
-
-    @Get()
-    findAll(): string {
-      return 'This action returns all pets';
-    }
+  
+  @Get()
+  findAll(): any[] {
+    return [
+      { id: 1, name: 'Fluffy', type: 'Cat' },
+      { id: 2, name: 'Buddy', type: 'Dog' },
+      { id: 3, name: 'Goldie', type: 'Fish' }
+    ];
+  }
 }
